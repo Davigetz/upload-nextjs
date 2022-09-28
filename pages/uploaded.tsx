d890bf39300bd0b952e3be7ffd270ed78cfbeefe
Davigetz <Davidstangetz13@gmail.com>
2022-09-28 18:34:19 -0500

    uploaded fixed

--- pages/uploaded.tsx
+++ pages/uploaded.tsx
@@ -8,7 +8,6 @@ import estilo from "../styles/drop.module.css";
 export const Uploaded = () => {
   const [buttonText, setButtonText] = useState("Copy Link");
   const pic = useAppSelector((state: RootState) => state.drops.pics);
-  console.log(pic);
   const router = useRouter();
   useEffect(() => {
     if (!pic.id) {
@@ -24,39 +23,41 @@ export const Uploaded = () => {
     }, 2000);
   };
   return (
-    <>
-      <div className={estilo.containerImgs}>
-        <h1 className={estilo.titulo}>Uploaded images</h1>
-        <div key={pic.id}>
-          <Image
-            src={pic.url!}
-            alt={pic.name!}
-            layout="intrinsic"
-            width={300}
-            height={300}
-          />
-          <div className={estilo.linked}>
-            <button
-              className={estilo.clicked}
-              type="button"
-              onClick={() => handleClick(pic.url!)}
-            >
-              {buttonText}
-            </button>
+    <div className={estilo.containerImgs}>
+      <h1 className={estilo.titulo}>Uploaded images</h1>
+      {pic.id !== null && (
+        <div>
+          <div key={pic.id}>
+            <Image
+              src={pic.url!}
+              alt={pic.name!}
+              layout="intrinsic"
+              width={300}
+              height={300}
+            />
+            <div className={estilo.linked}>
+              <button
+                className={estilo.clicked}
+                type="button"
+                onClick={() => handleClick(pic.url!)}
+              >
+                {buttonText}
+              </button>
+            </div>
           </div>
-        </div>
 
-        <button
-          onClick={(e: React.FormEvent) => {
-            e.preventDefault();
-            router.push("/");
-          }}
-          className={estilo.clicked}
-        >
-          Go Back
-        </button>
-      </div>
-    </>
+          <button
+            onClick={(e: React.FormEvent) => {
+              e.preventDefault();
+              router.push("/");
+            }}
+            className={estilo.clicked}
+          >
+            Go Back
+          </button>
+        </div>
+      )}
+    </div>
   );
 };
 
